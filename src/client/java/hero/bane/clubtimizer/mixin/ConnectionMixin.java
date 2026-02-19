2d361cc0ab75e5bfde8bda9e8829512a86d586ea
srira <srirambnair@gmail.com>
2026-02-18 21:58:42 -0500

    Removed killing interaction - caused bugs in duels with multiple rounds

--- src/client/java/hero/bane/clubtimizer/mixin/ConnectionMixin.java
+++ src/client/java/hero/bane/clubtimizer/mixin/ConnectionMixin.java
@@ -6,15 +6,10 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.network.Connection;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ServerboundInteractPacket;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.Interaction;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
-import net.minecraft.world.phys.Vec3;
-import net.minecraft.world.level.gameevent.GameEvent;
-import org.jspecify.annotations.NonNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -22,39 +17,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Connection.class)
 public class ConnectionMixin {
-
-    @Inject(method = "send(Lnet/minecraft/network/protocol/Packet;)V", at = @At("HEAD"))
-    private void club$interactionRemover(Packet<?> packet, CallbackInfo ci) {
-        Minecraft mc = Minecraft.getInstance();
-
-        if (MCPVPStateChanger.inGame() && packet instanceof ServerboundInteractPacket interactPacket) {
-            interactPacket.dispatch(new ServerboundInteractPacket.Handler() {
-                @Override
-                public void onInteraction(@NonNull InteractionHand hand) {
-                }
-
-                @Override
-                public void onInteraction(@NonNull InteractionHand hand, @NonNull Vec3 pos) {
-                }
-
-                @Override
-                public void onAttack() {
-                    HitResult hitResult = mc.hitResult;
-                    if (hitResult == null) return;
-
-                    if (hitResult.getType() == HitResult.Type.ENTITY) {
-                        Entity entity = ((EntityHitResult) hitResult).getEntity();
-                        if (entity instanceof Interaction) {
-                            if (mc.player == null) return;
-                            entity.remove(Entity.RemovalReason.KILLED);
-                            entity.gameEvent(GameEvent.ENTITY_DIE);
-                        }
-                    }
-                }
-            });
-        }
-    }
-
     @Inject(method = "send(Lnet/minecraft/network/protocol/Packet;)V", at = @At("HEAD"))
     private void club$onAttack(Packet<?> packet, CallbackInfo ci) {
         Minecraft mc = Minecraft.getInstance();
